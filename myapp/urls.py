0c0f6fccf1c83c343d63f2f9803f7ccf393453af
devsrakib <devs.rakibulislam@gmail.com>
2024-10-14 21:08:36 +0600

    updated

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from myapp import views
 
-urlpatterns = [path("contact_list/", views.contact_list)]
+urlpatterns = [
+    path("contact_list/", views.contact_list),
+    path("contact_detail/<int:pk>/", views.contact_detail),
+]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from myapp.models import Contact
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from myapp.serializers import ContactSerializer
@@ -21,3 +21,27 @@ def contact_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def contact_detail(request, pk):
+    try:
+        dvar = Contact.objects.get(pk=pk)
+    except dvar.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = ContactSerializer(dvar)
+        return JsonResponse(serializer.data)
+
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = ContactSerializer(dvar, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == "DELETE":
+        Contact.delete()
+        return HttpResponse(status=204)
