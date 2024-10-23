08fee4af287079257c1db7f1fa68704eaa9413d6
devsrakib <devs.rakibulislam@gmail.com>
2024-10-23 17:12:22 +0600

    updated

--- apiproject/urls.py
+++ apiproject/urls.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("myapp.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("api/v1/", include("myapp.urls"))]
